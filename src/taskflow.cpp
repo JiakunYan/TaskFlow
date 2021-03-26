56f9329164fbddff6e5cc94027a9b4be6fcffb45
Jiakun Yan <jiakuny3@illinois.com>
2021-03-26 17:27:25 +0800

    v0.2: support custom taskIdx

--- src/taskflow.cpp
+++
@@ -1,5 +0,0 @@
-#include "tf.hpp"
-
-namespace tf {
-std::atomic<int> Taskflow::total_taskflow_num(0);
-}
--- src/taskpool.hpp
+++ src/taskpool.hpp
@@ -1,48 +1,26 @@
-#ifndef TASKFRAMEWORK_TASKPOOL_HPP
-#define TASKFRAMEWORK_TASKPOOL_HPP
+#ifndef TASKFLOW_TASKPOOL_HPP
+#define TASKFLOW_TASKPOOL_HPP
 
 namespace tf {
 /**
- * @brief Taskpool is used to manage all the task dependency. It decides which
- * tasks are ready based on already completed tasks.
+ * @brief Taskpool is used to manage all ready tasks.
  *
  * Taskpool is shared by all xstreams.
  */
 class Taskpool {
 public:
   Taskpool(Context* context_, int nxstreams_);
-  /**
-   * @brief mark a task as completed
-   * @param[in] task the completed task
-   */
-  void markAsCompleted(Taskflow& taskflow, TaskIdx taskIdx);
 
-  /**
-   * @brief signal a task
-   * @param task the task to be signaled
-   * @return whether the task becomes ready
-   */
-  bool signal(Taskflow& taskflow, TaskIdx taskIdx);
+  Task *getNextReadyTask();
+  void putReadyTask(Task *task);
 
 private:
-  int nxstreams;
   Context *context;
+  int nxstreams;
 
-  struct pair_hash {
-    template <class T1, class T2>
-    std::size_t operator () (const std::pair<T1,T2> &p) const {
-      auto h1 = std::hash<T1>{}(p.first);
-      auto h2 = std::hash<T2>{}(p.second);
-
-      // Mainly for demonstration purposes, i.e. works but is overly simple
-      // In the real world, use sth. like boost.hash_combine
-      return h1 ^ h2;
-    }
-  };
-
-  using DepMap = std::unordered_map<std::pair<int, TaskIdx>, int, pair_hash>;
-  DepMap depCounter;
-  std::mutex depCounterLock;
+  using PQueue = std::priority_queue<Task *, std::vector<Task *>, Task::OpLess>;
+  PQueue readyTasks;
+  std::mutex readyTasksLock;
 };
 }
-#endif // TASKFRAMEWORK_TASKPOOL_HPP
+#endif // TASKFLOW_TASKPOOL_HPP
